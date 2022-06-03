938dce7785ea30da08688b20c39b08e3d1daa98f
MacBook <aleeshabaig1@gmail.com>
2022-06-03 23:18:16 +0500

    my first change

---
+++ tests/tests.js
@@ -0,0 +1,16 @@
+const { browser, element } = require("protractor");
+
+describe('angularjs homepage', function() {
+    it('should greet the named user', function() {
+      // Load the AngularJS homepage.
+      browser.get('http://www.angularjs.org');
+      browser.sleep(5000);
+    });
+    it('firstTestcase',function(){
+      browser.get('httlp://www.google.com');
+      element(by.)
+
+    });
+
+  });
+  
